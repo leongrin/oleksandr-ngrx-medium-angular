8365acd1d6eeccd1ffd29a415c1d2036c24960ca
Leon Grin <41086608+leongrin@users.noreply.github.com>
2021-10-25 16:15:35 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {StoreModule} from '@ngrx/store'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {AuthModule} from 'src/app/auth/auth.module'
+import {environment} from 'src/environments/environment'
+import {EffectsModule} from '@ngrx/effects'
+import {TopBarModule} from 'src/app/shared/modules/topBar/topBar.module'
+import {PersistanceService} from './shared/services/persistance.service'
+import {AuthInterceptor} from './shared/services/authinterceptor.service'
+import {GlobalFeedModule} from './globalFeed/globalFeed.module'
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([]),
+    TopBarModule,
+    GlobalFeedModule
+  ],
+  providers: [
+    PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -0,0 +1,65 @@
+import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {Store, select} from '@ngrx/store';
+import {getFeedAction} from 'src/app/shared/modules/feed/store/actions/getFeed.action';
+import {Observable, Subscription} from 'rxjs';
+import {GetFeedResponseInterface} from 'src/app/shared/modules/feed/types/getFeedResponse.interface';
+import {
+  feedSelector,
+  errorSelector,
+  isLoadingSelector
+} from 'src/app/shared/modules/feed/store/selectors';
+import {environment} from 'src/environments/environment';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'mc-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
+})
+export class FeedComponent implements OnInit, OnDestroy {
+  @Input('apiUrl') apiUrlProps: string;
+
+  feed$: Observable<GetFeedResponseInterface | null>;
+  error$: Observable<string | null>;
+  isLoading$: Observable<boolean>;
+  limit = environment.limit;
+  baseUrl: string;
+  queryParamsSubscription: Subscription;
+  currentPage: number;
+
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.initializeValues();
+    this.initializeListeners();
+    this.fetchData();
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe();
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params.page || '1');
+      }
+    );
+  }
+
+  initializeValues(): void {
+    this.feed$ = this.store.pipe(select(feedSelector));
+    this.error$ = this.store.pipe(select(errorSelector));
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.baseUrl = this.router.url.split('?')[0];
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getFeedAction({url: this.apiUrlProps}));
+  }
+}
